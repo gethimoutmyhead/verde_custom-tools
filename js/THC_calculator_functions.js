4e23051b975ee97dd11b12a48999738f9ec8aa6f
gethimoutmyhead <theshashiman@gmail.com>
2026-03-18 17:03:51 +0800

    functions to automatically add up totals as you type, are now functional

---
+++ js/THC_calculator_functions.js
@@ -0,0 +1,73 @@
+function dict_calculateDryHerbTotals(listOfDOMElems_dryHerbScripts){
+	// returns: {THCTotal: ###, 'dryWeight': ###}
+
+}
+
+function dict_calculateVapeTotals(listOfDOMElems_vapeScripts){
+	// returns: {THCTotal: ###, 'qty': ###}
+}
+
+function dict_calculateCannabisOilTotals(listOfDOMElems_oilScripts){
+	// returns: {THCTotal: ###, 'volume': ###}
+}
+
+function dict_calculateEdibleTotals(listOfDOMElems_edibleScripts){
+	// returns: {THCTotal: ###, 'qty': ###}
+}
+
+function dict_readAndsumTHCContentInScript(formElem_script){
+	// takes any valid form Elem and returns
+	// {
+
+	// 	'productType': 'text',
+	// 	'THCConversionFactor': ###,
+	// 	'productName': 'text', 
+	// 	'strength': ###,
+	// 	'unitQty': ###,
+	// 	'unitsPerDispense': ###,
+	// 	'repeats': ###,
+	// 	'sumTHCTotal': ###, 
+	// 	'sumQty': ###, 
+	// 	'unitMeasure': 'text',
+
+	// }
+	// unitQty, if not found, is assumed 1 (eg vials, pastilles)
+	// unitMeasure can be ['g', 'vials', 'mL', 'units']
+	// sumTHCTotal, sumQty, are calculated
+	// unitMeasure can be ['g', 'vials', 'mL', 'doses']
+	const scriptDetails = new Object();
+	// scriptDetails['productName'] = formElem_script.getAttribute('productName')
+	key1 = ['productType','unitMeasure']
+	key2 = ['THCConversionFactor']
+	key3 = ['productName']
+	key4 = ['strength', 'unitsPerDispense', 'repeats']
+	det1 = key1.map(elem => {
+		return formElem_script.getAttribute(elem);
+	})
+	det2 = key2.map(elem => {
+		return Number(formElem_script.getAttribute(elem));
+	})
+	det3 = key3.map(elem => {
+		return formElem_script.querySelector(`.${elem}`).value;
+	})
+	det4 = key4.map(elem => {
+		return Number(formElem_script.querySelector(`.${elem}`).value);
+	})
+	key = key1.concat(key2, key3, key4)
+	det = det1.concat(det2, det3, det4)
+
+	key.forEach((elem, idx) => {
+		scriptDetails[elem] = det[idx]
+	});
+	scriptDetails['unitQty'] = formElem_script.querySelector('.unitQty') ? formElem_script.querySelector('.unitQty').value : 1;
+	scriptDetails['sumQty'] = scriptDetails['unitsPerDispense'] * scriptDetails['unitQty'] * ((scriptDetails['repeats']) + 1)
+	scriptDetails['sumTHCTotal'] = scriptDetails['sumQty'] * scriptDetails['strength'] * eval(scriptDetails['THCConversionFactor'])
+	return (scriptDetails)
+}
+
+
+
+function updateTHCContentInScript(formElem_script, scriptSumAndDetail = null){
+	const details = (scriptSumAndDetail) ? scriptSumAndDetail : dict_readAndsumTHCContentInScript(formElem_script)
+	formElem_script.querySelector('.THCTotal').innerText = `${details['sumTHCTotal']} mg`
+}
--- js/init.js
+++ js/init.js
@@ -1,1 +1,40 @@
 document.getElementById('prescriptionDate').valueAsDate = new Date();
+
+// [...document.querySelectorAll('.scriptForm')].forEach(scriptForm => {
+// 	updateTHCContentInScript(scriptForm)
+// 	scriptForm.addEventListener('input', () => {
+// 		updateTHCContentInScript(scriptForm)
+// 	})
+// })
+
+['dryHerbTable', 'vapeCartridgeTable','cannabisOilTable','ediblesTable'].forEach(scriptList =>
+	{
+		const scriptListDOM = document.getElementById(scriptList)
+		const arrayOfScriptDOMs = [...scriptListDOM.querySelectorAll('.scriptForm')]
+		arrayOfScriptObjects = arrayOfScriptDOMs.map(dict_readAndsumTHCContentInScript)
+
+		const THCSums = arrayOfScriptObjects.reduce((sumTHC, scriptObject) => scriptObject['sumTHCTotal'] + sumTHC, 0,)
+		const sumTotalQty = arrayOfScriptObjects.reduce((sumQty, scriptObject) => scriptObject['sumQty'] + sumQty, 0,)
+		scriptListDOM.querySelector('.sumUnitTotals').innerText = `${sumTotalQty} ${arrayOfScriptObjects[0]['unitMeasure']}`
+		scriptListDOM.querySelector('.sumTHCTotals').innerText = `${THCSums} mg`
+
+		// console.log(`${scriptList} - ${THCSums}mg - ${sumTotalQty} ${arrayOfScriptObjects[0]['unitMeasure']}`)
+		// console.log(arrayOfScriptObjects[0])
+
+
+		arrayOfScriptDOMs.forEach( (scriptDOM, idx) => {
+			updateTHCContentInScript(scriptDOM, arrayOfScriptObjects[idx])
+		})
+
+		scriptListDOM.addEventListener('input', () => {
+			updateCalculationsInTable(scriptList)
+		})
+
+		// arrayOfScriptDOMs.forEach(scriptDOM => {
+		// 	// updateTHCContentInScript(scriptForm)
+		// 	scriptDOM.addEventListener('input', () => {
+		// 		updateTHCContentInScript(scriptDOM)
+		// 	})
+		// })
+		// add code to sum all the individual script tables here
+	})
